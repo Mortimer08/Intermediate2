2eec40b976c36f48cf67987786ba695b0005a34c
Ilya Makarov <ilyam2003@mail.ru>
2023-05-14 11:07:08 +0500

    Lottery Algorithm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,37 @@ public class Main {
     public static void main(String[] args) {
         Toy a = new Toy("Toy1", 2);
         Toy b = new Toy("Toy2", 5);
-        a.setChance(50);
-        b.setChance(30);
+        Toy c = new Toy("Toy3", 5);
+        a.setChance(10);
+        b.setChance(10);
+        c.setChance(10);
         ToyStorage ts = new ToyStorage();
         ts.addToy(a);
         ts.addToy(b);
+        ts.addToy(c);
 //        ts.pullToy(a);
 
 //        System.out.println(a.getQuantity());
-//        Lottery l = new Lottery(ts);
-//        l.selectPrize();
-        System.out.println(a);
+        Integer wins[] = {0, 0, 0};
+        Lottery l = new Lottery(ts);
+        l.selectPrize();
         System.out.println(ts);
-        ts.sortByChance();
-        System.out.println(ts);
+        for (Integer i = 0; i < 100; i++) {
+            Toy prize = l.selectPrize();
+//            System.out.println(prize);
+            if (prize.equals(a)) {
+                wins[0] += 1;
+            } else if (prize.equals(b)) {
+                wins[1] += 1;
+            } else if (prize.equals(c)) {
+                wins[2] += 1;
+            }
+        }
+
+        System.out.println(String.format("%d %d %d",wins[0], wins[1], wins[2]));
+//        System.out.println(a);
+//        System.out.println(ts);
+//        ts.sortByChance();
+//        System.out.println(ts);
     }
 }
--- src/main/java/org/example/ToyStorage.java
+++ src/main/java/org/example/ToyStorage.java
@@ -1,44 +1,49 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Consumer;
 
-public class ToyStorage {
+public class ToyStorage implements Iterable<Toy> {
     //    private int storageId;
     private Comparator comparator;
-    private Map<String, Toy> toys;
+//    private Map<String, Toy> toys;
     private ArrayList<Toy> toysList;
+    private int iteratorIndex;
 
     public ToyStorage() {
-        toys = new HashMap<String, Toy>();
+//        toys = new HashMap<String, Toy>();
         toysList = new ArrayList<>();
         comparator = new ToyChanceComparator();
+        iteratorIndex = 0;
     }
 
     public void addToy(Toy toy) {
-        if (toys.values().contains(toy)) {
-            toy.setQuantity(toy.getQuantity() + 1);
-        }
-        toys.put(toy.getId(), toy);
+//        if (toys.values().contains(toy)) {
+//            toy.setQuantity(toy.getQuantity() + 1);
+//        }
+//        toys.put(toy.getId(), toy);
         toysList.add(toy);
     }
 
-    public void putToy(Toy toy) {
+//    public void putToy(Toy toy) {
+//
+//    }
 
-    }
-
-    public Toy getToy(String id) {
-//        toys.
-        return toys.get(id);
-    }
+//    public Toy getToy(String id) {
+////        toys.
+//        return toys.get(id);
+//    }
 
     public Toy pullToy(Toy toy) {
         toy.setQuantity(toy.getQuantity() - 1);
-        return toys.get(toy.getId());
+        return toy;
     }
-
+    public Toy pullLastToy() {
+        toysList.sort(this.comparator);
+        Toy lastToy =toysList.get(toysList.size()-1);
+        lastToy.setQuantity(lastToy.getQuantity()-1);
+        return lastToy;
+    }
     public void sortByChance() {
         toysList.sort(comparator);
     }
@@ -46,10 +51,27 @@ public class ToyStorage {
     @Override
     public String toString() {
         StringBuilder storageString = new StringBuilder();
-        for (Toy toy: this.toysList) {
+        for (Toy toy : this.toysList) {
             storageString.append(toy.toString());
         }
         return storageString.toString();
+    }
+
+    class ToyStorageIterator implements Iterator<Toy> {
+        @Override
+        public boolean hasNext() {
+            return iteratorIndex != toysList.size();
+        }
+
+        @Override
+        public Toy next() {
+            return toysList.get(iteratorIndex++);
+        }
+    }
+
+    @Override
+    public Iterator<Toy> iterator() {
+        return new ToyStorageIterator();
     }
 //    public int getQuantity(Toy toy){
 //        int quantity;
